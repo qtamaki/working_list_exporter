a6a1e4191e5fa17cb557d2306166560fa4bda50a
Tatsunori TAMAKI <github@qtamaki.com>
2013-09-10 07:00:03 +0900

    POI追加

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -5,9 +5,12 @@ object Dependencies {
   val scalatest = "org.scalatest" %% "scalatest" % "1.9.1"
   val junit = "junit" % "junit" % "4.11"
 
+  val poi = "org.apache.poi" % "poi" % "3.9"
+
   val appDependencies = Seq(
     scalatest % "test",
-    junit % "test"
+    junit % "test",
+    poi
   )
 
 }
